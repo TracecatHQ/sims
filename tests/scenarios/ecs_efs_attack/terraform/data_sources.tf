22a93ad7dbe3d00e45b123c4d0caf6ab4c0dc541
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2024-01-27 08:17:41 +0000

    test: Delete cloudgoat in tests

--- tests/scenarios/ecs_efs_attack/terraform/data_sources.tf
+++
@@ -1,9 +0,0 @@
-#AWS Account Id
-data "aws_caller_identity" "aws-account-id" {}
-
-locals {
-  default_tags = {
-    Stack    = var.stack-name
-    Scenario = var.scenario-name
-  }
-}
--- tests/scenarios/rce_web_app/assets/rce_app/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: 'airbnb-base',
-  parser: 'babel-eslint',
-  plugins: ['babel'],
-  rules: {
-    'no-plusplus': 0
-  },
-  globals: {},
-};
