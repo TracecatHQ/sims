2a82e9fa223b273763b21fa54f046a07a8916fa6
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2024-01-28 06:04:20 +0000

    awd

--- helloworldkitty/config.py
+++ helloworldkitty/config.py
@@ -1,10 +1,13 @@
 import os
 from pathlib import Path
 
+LOGS_FILE_TIMESTAMP_FORMAT = "%Y%m%d%H%M%S"
 HWK__HOME_DIR = (
     Path(os.environ.get("HWK__HOME_DIR", "~/.helloworldkitty")).expanduser().resolve()
 )
 HWK__LAB_DIR = HWK__HOME_DIR / "lab"
 HWK__ALERTS_DIR = HWK__HOME_DIR / "alerts"
+HWK__TRIAGE_DIR = HWK__HOME_DIR / "triage"
+HWK__LOGS_DIR = HWK__HOME_DIR / "logs"  # Nice clean normalized logs
 HWK__DOCKER_COMPOSE_NAME = "hwk-lab"
 HWK__DOCKER_WORKDIR = "/home/terraform/lab"
