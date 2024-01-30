44a0e828c4acb8c871fd15767d5dadc589bf8bbc
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2024-01-30 23:31:29 +0000

    feat: Add user ids getters

--- tracecat/credentials.py
+++ tracecat/credentials.py
@@ -6,11 +6,26 @@ def load_lab_credentials(is_compromised: bool = False):
     with open(TRACECAT__LAB_DIR / "terraform/credentials.json") as f:
         key_type = "compromised" if is_compromised else "normal"
         try:
-            creds = json.load(f)
-            selected_creds = creds[key_type]
+            creds = json.load(f)[key_type]
         except KeyError as err:
             raise KeyError(
                 "Expected keys grouped as `compromised` or `normal`."
-                f"Found {selected_creds.keys()!r} instead."
+                f"Found {creds.keys()!r} instead."
             ) from err
     return creds
+
+
+def get_normal_ids() -> list[str]:
+    normal_ids = [
+        creds["aws_access_key_id"] for creds in
+        load_lab_credentials(is_compromised=False).values()
+    ]
+    return normal_ids
+
+
+def get_malicious_ids() -> list[str]:
+    malicious_ids = [
+        creds["aws_access_key_id"] for creds in
+        load_lab_credentials(is_compromised=True).values()
+    ]
+    return malicious_ids
