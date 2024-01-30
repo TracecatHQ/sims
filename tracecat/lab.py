2ec3f4ea1a1430527f4a2c6b1e538f05274a0286
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2024-01-30 05:08:16 +0000

    feat: Rename cli to lab

--- tracecat/lab.py
+++ tracecat/lab.py
@@ -2,9 +2,6 @@ import os
 import shutil
 from datetime import datetime
 
-import typer
-from typing_extensions import Annotated
-
 from tracecat.attack.ddos import ddos as signal_ddos
 from tracecat.config import TRACECAT__TRIAGE_DIR
 from tracecat.defense.siem_alerts import get_datadog_alerts
@@ -18,26 +15,17 @@ from tracecat.ingestion.aws_cloudtrail import (
 )
 from tracecat.setup import cleanup_lab, deploy_lab, initialize_lab
 
-app = typer.Typer()
-cleanup_app = typer.Typer(help="Cleanup lab, logs, and resources.")
-app.add_typer(cleanup_app, name="cleanup")
 
-
-@app.command()
-def warmup(
-    project_path: Annotated[str, typer.Argument(help="Path to Terraform project")],
-):
+def warmup(scenario_id: str):
     """Create lab ready for attacks."""
-    initialize_lab(project_path=project_path)
-    deploy_lab(project_path=project_path)
+    initialize_lab(scenario_id=scenario_id)
+    deploy_lab()
 
 
-@app.command(help="Denotate attacks.")
 def ddos(n_attacks: int = 10, delay: int = 1):
     signal_ddos(n_attacks=n_attacks, delay=delay)
 
 
-@app.command(help="Diagnose your SIEM.")
 def diagnose(
     start: datetime,
     end: datetime,
@@ -76,27 +64,6 @@ def diagnose(
     compute_confusion_matrix(correlated_alerts)
 
 
-@cleanup_app.command(help="Destroy lab.")
-def lab():
-    delete = typer.confirm(
-        "Are you sure you want to destroy all resources and scripts in your lab?"
-    )
-    if not delete:
-        print("Cleanup aborted")
-        raise typer.Abort()
-    print("🧹 Run lab cleanup")
+def cleanup():
     cleanup_lab()
-
-
-@cleanup_app.command(help="Delete triaged logs")
-def logs():
-    delete = typer.confirm("Are you sure you want to delete all triaged logs?")
-    if not delete:
-        print("Logs deletion aborted")
-        raise typer.Abort()
-    print("🧹 Run logs deletion")
     shutil.rmtree(TRACECAT__TRIAGE_DIR)
-
-
-if __name__ == "__main__":
-    app()
