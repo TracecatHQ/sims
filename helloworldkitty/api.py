ad3987d9424b0a2d270ad933614405f6d6f0afd8
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2024-01-30 01:22:09 +0000

    feat: Add file logging API

---
+++ helloworldkitty/api.py
@@ -0,0 +1,205 @@
+from __future__ import annotations
+
+import asyncio
+import json
+from contextlib import asynccontextmanager
+from pathlib import Path
+from typing import Annotated, Any, Literal, Optional
+
+from dotenv import find_dotenv, load_dotenv
+from fastapi import Depends, FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import ORJSONResponse
+from pydantic import BaseModel
+from sse_starlette.sse import EventSourceResponse
+
+from helloworldkitty.config import HWK__HOME_DIR, TRACECAT__API_HOME_DIR
+from helloworldkitty.logger import standard_logger, tail_file
+
+load_dotenv(find_dotenv())
+logger = standard_logger(__name__)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Context manager to run the API for its lifespan."""
+    logger.info("Starting API")
+    asyncio.create_task(tail_file_handler())
+    asyncio.create_task(update_stats_handler())
+    yield
+
+
+app = FastAPI(debug=True, default_response_class=ORJSONResponse, lifespan=lifespan)
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+    "http://localhost:3001",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+ENABLED_OPTIONAL_FEATURES = {}
+
+
+def safe_json_load(path: Path) -> dict:
+    if not path.exists():
+        path.parent.mkdir(parents=True, exist_ok=True)
+        path.touch(exist_ok=True)
+        return {}
+    elif path.stat().st_size == 0:
+        return {}
+    with path.open("r") as f:
+        return json.load(f)
+
+
+#################
+### ENDPOINTS ###
+#################
+
+
+@app.get("/")
+def root():
+    return {"status": "ok"}
+
+
+queues: dict[str, asyncio.Queue] = {}
+for q in ["stats", "activity"]:
+    queues[q] = asyncio.Queue()
+
+
+JobStatus = Literal["success", "error"]
+
+
+async def tail_file_handler():
+    log_file = HWK__HOME_DIR / "logs/test.log"
+
+    async for line in tail_file(log_file):
+        line = line.strip()
+        for _, q in queues.items():
+            await q.put(line)
+
+
+api_call_stats_file_lock = asyncio.Lock()
+action_stats_file_lock = asyncio.Lock()
+
+bad_usernames = {"root", "admin", "test", "user"}
+
+
+async def update_stats_handler():
+    api_call_stats_file = TRACECAT__API_HOME_DIR / "api_calls.json"
+    action_stats_file = TRACECAT__API_HOME_DIR / "action_stats.json"
+
+    api_call_stats_file.touch(exist_ok=True)
+    action_stats_file.touch(exist_ok=True)
+    while True:
+        item = await queues["stats"].get()
+        item = json.loads(item)
+        async with api_call_stats_file_lock:
+            data = safe_json_load(api_call_stats_file)
+
+            # update api call frequency
+            action = item["action"]
+            data[action] = data.get(action, 0) + 1
+            with api_call_stats_file.open("w") as f:
+                json.dump(data, f)
+
+        if item["user"] not in bad_usernames:
+            continue
+        async with action_stats_file_lock:
+            # Update total api calls per user
+            # If user bad update bad user count
+            data = safe_json_load(action_stats_file)
+
+            # update bad api call frequency
+            data["bad_api_calls_count"] = data.get("bad_api_calls_count", 0) + 1
+            action = item["action"]
+            data[action] = data.get(action, 0) + 1
+            with api_call_stats_file.open("w") as f:
+                json.dump(data, f)
+
+
+@app.get("/activity-stream", response_class=EventSourceResponse)
+async def stream_logs():
+    async def log_stream():
+        while True:
+            item = await queues["activity"].get()
+            yield item
+
+    return EventSourceResponse(log_stream())
+
+
+class StatsFeedUpdate(BaseModel):
+    """
+    Represents a stats feed update.
+
+    Attributes:
+        time (datetime): The time of the update.
+        data (dict[str, Any]): The data of the update.
+    """
+
+    id: str
+    title: str
+    value: Any
+    description: str
+    units: Optional[str] = None
+    data: Optional[dict[str, Any]] = None
+
+
+# Polling stats
+def validate_stat_id(id: str):
+    if not len(id) == 9 or not id.startswith("STAT-"):
+        raise HTTPException(status_code=400, detail="Invalid stat ID")
+    return id
+
+
+@app.get("/stats-feed/{id}")
+async def get_stats(id: Annotated[str, Depends(validate_stat_id)]):
+    stats_path = TRACECAT__API_HOME_DIR / "stats.json"
+    if id == "STAT-0005":
+        data = safe_json_load(TRACECAT__API_HOME_DIR / "api_calls.json")
+
+        return {
+            "id": "STAT-0005",
+            "title": "Total actions taken",
+            "value": sum(data.values()),
+            "description": "",
+        }
+    elif id == "STAT-0006":
+        data = safe_json_load(TRACECAT__API_HOME_DIR / "action_stats.json")
+
+        denom = sum(data.values())
+        value = 0 if denom == 0 else data.get("bad_api_calls_count", 0) / denom * 100
+        return {
+            "id": "STAT-0006",
+            "title": "% of actions used by adversaries",
+            "value": value,
+            "units": "%",
+            "description": "",
+        }
+    stats = safe_json_load(stats_path)
+    for item in stats:
+        if item["id"] == id:
+            return item
+    raise HTTPException(status_code=404, detail="Stat ID not found")
+
+
+def validate_graph_id(id: str):
+    if not len(id) == 10 or not id.startswith("GRAPH-"):
+        raise HTTPException(status_code=400, detail="Invalid stat ID")
+    return id
+
+
+@app.get("/graph-feed/{id}")
+async def get_graph_feed(id: Annotated[str, Depends(validate_graph_id)]):
+    """Returns a graph feed update."""
+    path = TRACECAT__API_HOME_DIR / "api_calls.json"
+    calls = safe_json_load(path)
+    return calls
--- helloworldkitty/logger.py
+++ helloworldkitty/logger.py
@@ -1,29 +1,78 @@
+import asyncio
+import json
 import logging
 import os
 from pathlib import Path
+from typing import Literal
 
-from helloworldkitty.config import HWK__HOME_DIR
-
-LOG_FILE = HWK__HOME_DIR / "hwk.log"
-LOG_FILE.parent.mkdir(parents=True, exist_ok=True)
-LOG_FILE.touch(exist_ok=True)
+from pydantic import BaseModel
 
 LOG_FORMAT = (
     "%(asctime)s - [%(levelname)s] - %(name)s::%(funcName)s(%(lineno)d) - %(message)s"
 )
 
 
-def file_logger(name: str, level: int | str | None = None) -> logging.Logger:
+def log_id_gen():
+    i = 0
+    while True:
+        yield f"LOG-{i:04d}"
+        i += 1
+
+
+class Log(BaseModel):
+    id: str
+    title: str
+    user: str
+    action: str
+    description: str | None = None
+
+
+class JsonFormatter(logging.Formatter):
+    """Custom formatter to output logs in JSON format."""
+
+    _date_format = "%Y-%m-%dT%H:%M:%SZ"
+
+    def _get_json_msg(self, record: logging.LogRecord) -> str:
+        if isinstance(record.msg, dict):
+            return json.dumps(record.msg)
+        if isinstance(record.msg, BaseModel):
+            return record.msg.model_dump_json()
+        raise ValueError("Log message must be JSON serializable.")
+
+    def format(self, record: logging.LogRecord):
+        model_dict = self._get_json_msg(record)
+        model_dict["time"] = self.formatTime(record, self._date_format)
+        return json.dumps(model_dict)
+
+
+LOG_FORMATTER_FACTORY = {
+    "json": JsonFormatter,
+    "log": logging.Formatter,
+}
+
+
+def file_logger(
+    name: str,
+    file_path: str,
+    level: int | str | None = None,
+    format: Literal["json", "log"] | None = None,
+) -> logging.Logger:
     """Sets up a logger that logs messages to a file."""
     logger = logging.getLogger(name)
+    format = format or "log"
+    formatter = LOG_FORMATTER_FACTORY[format]
 
     # Set logger level
     resolved_level = level or os.getenv("LOG_LEVEL", "INFO")
     logger.setLevel(resolved_level)
 
     # Create file handler
-    file_handler = logging.FileHandler(str(LOG_FILE))
-    file_handler.setFormatter(logging.Formatter(LOG_FORMAT))
+    file_path = Path(file_path)
+    file_path.parent.mkdir(parents=True, exist_ok=True)
+    file_path.touch(exist_ok=True)
+
+    file_handler = logging.FileHandler(file_path)
+    file_handler.setFormatter(formatter())
 
     # Add the handler to the logger
     logger.addHandler(file_handler)
@@ -31,9 +80,15 @@ def file_logger(name: str, level: int | str | None = None) -> logging.Logger:
     return logger
 
 
-def standard_logger(name: str, level: int | str | None = None) -> logging.Logger:
+def standard_logger(
+    name: str,
+    level: int | str | None = None,
+    format: Literal["json", "log"] | None = None,
+) -> logging.Logger:
     """Sets up a logger that logs messages to the console."""
     logger = logging.getLogger(name)
+    format = format or "log"
+    formatter = LOG_FORMATTER_FACTORY[format]
 
     # Set logger level
     resolved_level = level or os.getenv("LOG_LEVEL", "INFO")
@@ -41,7 +96,7 @@ def standard_logger(name: str, level: int | str | None = None) -> logging.Logger
 
     # Create stream handler
     stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(logging.Formatter(LOG_FORMAT))
+    stream_handler.setFormatter(formatter())
 
     # Add the handler to the logger
     logger.addHandler(stream_handler)
@@ -49,16 +104,40 @@ def standard_logger(name: str, level: int | str | None = None) -> logging.Logger
     return logger
 
 
-class TqdmToFile:
-    def __init__(self, file_path: Path):
-        self.file_path = file_path
+def composite_logger(
+    name: str,
+    level: int | str | None = None,
+    file_paths: list[str | Path] | None = None,
+    format: Literal["json", "log"] | None = None,
+) -> logging.Logger:
+    """Log to stdout and file(s)"""
+    logger = logging.getLogger(name)
+    logger.setLevel(level or os.getenv("LOG_LEVEL", "INFO"))
+    format = format or "log"
+    formatter = LOG_FORMATTER_FACTORY[format]
 
-    def write(self, s):
-        with self.file_path.open("a") as f:
-            f.write(s)
+    # Files
+    if file_paths is not None:
+        for file in file_paths:
+            file = Path(file)
+            file.parent.mkdir(parents=True, exist_ok=True)
+            file.touch(exist_ok=True)
+            logger.addHandler(logging.FileHandler(str(file)))
 
-    def flush(self):
-        pass  # This is a no-op since we are immediately writing to the file
+    # Stderr
+    logger.addHandler(logging.StreamHandler())
+    for handler in logger.handlers:
+        handler.setFormatter(formatter())
+    return logger
 
 
-TQDM_FILE_HANDLER = TqdmToFile(LOG_FILE)
+async def tail_file(file_path: Path):
+    """Tail an NDJSON file and put new lines into a queue."""
+    with open(file_path, "r") as file:
+        file.seek(0, 2)  # Go to the end of the file
+        while True:
+            line = file.readline()
+            if not line:
+                await asyncio.sleep(0.1)  # Wait briefly
+                continue
+            yield line
