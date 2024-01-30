e2dafbb09ca994c925dd7e7de767ee18a456d757
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2024-01-30 04:40:55 +0000

    feat: Add lab credentials loader

---
+++ tracecat/credentials.py
@@ -0,0 +1,16 @@
+import json
+from tracecat.config import TRACECAT__LAB_DIR
+
+
+def load_lab_credentials(is_compromised: bool = False):
+    with open(TRACECAT__LAB_DIR / "credentials.json") as f:
+        key_type = "compromised" if is_compromised else "normal"
+        try:
+            creds = json.load(f)
+            selected_creds = creds[key_type]
+        except KeyError as err:
+            raise KeyError(
+                "Expected keys grouped as `compromised` or `normal`."
+                f"Found {selected_creds.keys()!r} instead."
+            ) from err
+    return creds
