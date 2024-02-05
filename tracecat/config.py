cae626fcae49c0ee12fec0559ce5377e6be73b58
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2024-02-05 01:51:40 +0000

    feat: Add `TRACECAT__THREAT_INTEL_DIR` drop docker vars

--- tracecat/config.py
+++ tracecat/config.py
@@ -4,7 +4,9 @@ from pathlib import Path
 
 LOGS_FILE_TIMESTAMP_FORMAT = "%Y%m%d%H%M%S"
 STRATUS__HOME_DIR = (
-    Path(os.environ.get("STRATUS__HOME_DIR", "~/.stratus-red-team")).expanduser().resolve()
+    Path(os.environ.get("STRATUS__HOME_DIR", "~/.stratus-red-team"))
+    .expanduser()
+    .resolve()
 )
 TRACECAT__HOME_DIR = (
     Path(os.environ.get("TRACECAT__HOME_DIR", "~/.tracecat")).expanduser().resolve()
@@ -14,11 +16,10 @@ TRACECAT__LAB_DIR = TRACECAT__HOME_DIR / "lab"
 TRACECAT__API_DIR = TRACECAT__HOME_DIR / "api"
 TRACECAT__RULES_DIR = TRACECAT__HOME_DIR / "rules"
 TRACECAT__ALERTS_DIR = TRACECAT__HOME_DIR / "alerts"
+TRACECAT__THREAT_INTEL_DIR = TRACECAT__HOME_DIR / "threat_intel"
 TRACECAT__TRIAGE_DIR = TRACECAT__HOME_DIR / "triage"
 TRACECAT__LOGS_DIR = TRACECAT__HOME_DIR / "logs"  # Nice clean normalized logs
 TRACECAT__AUTOTUNER_DIR = TRACECAT__HOME_DIR / "autotuner"
-TRACECAT__DOCKER_COMPOSE_NAME = "tracecat-lab"
-TRACECAT__DOCKER_WORKDIR = "/home/terraform/lab"
 
 TRACECAT__VECTORDB_DIR.mkdir(parents=True, exist_ok=True)
 TRACECAT__TRIAGE_DIR.mkdir(parents=True, exist_ok=True)
